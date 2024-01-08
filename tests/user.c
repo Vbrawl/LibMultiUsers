a4a5440038c9f4c2a898db74c66a7fd7aec8b55d
vbrawl <greninjagr@gmail.com>
2024-01-08 18:08:20 +0200

    Added a test case.

---
+++ tests/user.c
@@ -0,0 +1,25 @@
+#include <multiusers.h>
+
+// for this test to work you need to provide a username at the compilation time.
+// use -DTEST_FOR_USER=$(whoami)
+#ifndef TEST_FOR_USER
+#error "Provide a user using the option -DTEST_FOR_USER. eg.[bash] -DTEST_FOR_USER=$(whoami)"
+#endif
+
+#include <string.h>
+
+int main() {
+	int rc = 0;
+	user_data_t *ud;
+
+	rc |= get_user_data(TEST_FOR_USER, &ud);
+
+	if(rc == 0) {
+		if(strncmp(TEST_FOR_USER, ud->username, sizeof(TEST_FOR_USER)) != 0) {
+			rc |= -1;
+		}
+
+		clear_user_data(ud);
+	}
+	return rc;
+}
